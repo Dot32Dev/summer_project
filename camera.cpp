e204317ed7fde2dedbd26f02a45610bc857d9c17
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2025-11-29 19:11:10 +0800

    Some commenting in camera's implementation

--- camera.cpp
+++ camera.cpp
@@ -10,17 +10,23 @@ Camera::Camera(glm::vec3 pos) :
 
 glm::mat4 Camera::get_view_matrix() {
 	glm::mat4 matrix = glm::mat4(1.0);
+	// Rotate by the camera's dir
 	glm::vec3 pitch_axis = glm::vec3(1.0f, 0.0f, 0.0f);
 	matrix = glm::rotate(matrix, dir.y, pitch_axis);
 	glm::vec3 yaw_axis = glm::vec3(0.0f, 1.0f, 0.0f);
 	matrix = glm::rotate(matrix, dir.x, yaw_axis);
+	// Translate by the camera's pos
 	matrix = glm::translate(matrix, pos);
 	return matrix;
 }
 
 void Camera::pos_input(glm::vec3 input_vector) {
+	// Rotate the input by the direction the camera is looking
+	// This is so that if you walk forwards, you move forwards relative to the
+	// view, rather than the cardinal direction which is forwards.
 	glm::vec3 yaw_axis = glm::vec3(0.0f, -1.0f, 0.0f);
 	glm::mat4 rotation = glm::rotate(glm::mat4(1.0), dir.x, yaw_axis);
+	// This is just rotation * input, except rotation is mat4 and input is vec3
 	pos += glm::vec3(rotation * glm::vec4(input_vector, 1.0));
 }
 
